55a8b730ac0e1411d5164ba3e8a040c223597bc9
alan-d-mitchell <alan.mitchell@piusxi.org>
2025-03-12 19:52:47 -0500

    Added webiste directory

---
+++ Lockheed Martin/f22.js
@@ -0,0 +1,4 @@
+// Simple button click alert
+document.querySelector('.cta-button').addEventListener('click', () => {
+    alert('Welcome to the Lockheed Martin mission! Together, we secure the future.');
+});
